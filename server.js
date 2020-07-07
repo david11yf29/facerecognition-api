a369263d819803ee1519d11c3202f57e40b76fd5
david11yf29 <david11yf29@gmail.com>
2020-07-07 18:21:07 +0800

    updated

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express = requrie('express');
+const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('This is working')
+})
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
+
+// "/" --> res = this is working
+// /signin --> POST = success/fail
+// /register --> POST = user
+// /profile/:userId --> GET = user
+// /image --> PUT --> user
