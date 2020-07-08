a280f60f15ea482b7e85899f71c7686ff4e183a0
david11yf29 <david11yf29@gmail.com>
2020-07-08 15:12:51 +0800

    updated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
-app.use(express.json());
 
 const database = {
     users: [
@@ -31,6 +31,9 @@ const database = {
         }
     ]
 }
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send(database.users)
