e85b7343fc0b7faef49ba088ba113fefa60511cb
david11yf29 <david11yf29@gmail.com>
2020-07-12 16:34:06 +0800

    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = knex({
     client: 'pg',
     connection: {
         // localhost 
-        host : '127.0.0.1',
+        host : 'postgresql-elliptical-55499',
         user : 'DoDoTea',
         password : '',
         database : 'facerecognition-db'
