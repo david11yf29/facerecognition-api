ba7ce0399bad2d62ad109fd0d1dff1250849ce16
david11yf29 <david11yf29@gmail.com>
2020-07-12 16:42:04 +0800

    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = knex({
     client: 'pg',
     connection: {
         // localhost 
-        host : process.env.DATABASE_URL,
+        connectionString : process.env.DATABASE_URL,
         ssl: true
     }
 });
